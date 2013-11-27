4bfd3b5157ea173632c410f1a2167c6b174765f6
dobri-dobrev <dmd2169@columbia.edu>
2013-11-26 21:37:30 -0500

    yay

--- js/main.js
+++ js/main.js
@@ -1,4 +1,26 @@
 $("#q1").click(function(){
-
-	alert($("#q1yes").attr('checked'));
+	var t = $("#q1yes").is(":checked");
+	
+	if($("#q1yes").is(":checked")) {
+	
+		loadQuestion2();
+	}
+	else{
+		loadQuestion3();
+	}
 });
+
+function loadQuestion2(){
+	
+	$("#panel-title").html("Do you sleep well at night?");
+	$("#zeBody").html('<form action=""> <input id="q1yes" type="radio" name="ans" value="yes">&nbsp;Yes<br>'+
+                 '<input id="q1no" type="radio" name="ans" value="no">&nbsp;No'+
+                  '</form>'+
+                  '<button id="q2" style="float:right;" type="button" class="btn btn-primary">Answer</button>');
+
+	$("#q2").click(q2listener);
+}
+
+function q2listener(){
+	alert("profit");
+}
