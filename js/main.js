e7fce70c1d15888baedbe847bd837445c063ef3b
dobri-dobrev <dmd2169@columbia.edu>
2013-11-26 22:12:26 -0500

    embarrasing

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 $("#q1").click(function(){
 	var t = $("#q1yes").is(":checked");
-
+	
 	if($("#q1yes").is(":checked")) {
-
+	
 		loadQuestion2();
 	}
 	else{
@@ -11,7 +11,7 @@ $("#q1").click(function(){
 });
 
 function loadQuestion2(){
-
+	
 	$("#panel-title").html("Do you sleep well at night?");
 	$("#zeBody").html('<form action=""> <input id="q1yes" type="radio" name="ans" value="yes">&nbsp;Yes<br>'+
                  '<input id="q1no" type="radio" name="ans" value="no">&nbsp;No'+
@@ -19,6 +19,7 @@ function loadQuestion2(){
                   '<button id="q2" style="float:right;" type="button" class="btn btn-primary">Answer</button>');
 
 	$("#q2").click(q2listener);
+	console.log("")
 }
 
 function q2listener(){
