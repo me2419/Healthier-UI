1ef8f6555d111296da4798d8c4365a971fa498cc
Mehmet Erkilic <me2419@columbia.edu>
2013-11-26 21:40:53 -0500

    Space correction

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 $("#q1").click(function(){
 	var t = $("#q1yes").is(":checked");
-	
+
 	if($("#q1yes").is(":checked")) {
-	
+
 		loadQuestion2();
 	}
 	else{
@@ -11,7 +11,7 @@ $("#q1").click(function(){
 });
 
 function loadQuestion2(){
-	
+
 	$("#panel-title").html("Do you sleep well at night?");
 	$("#zeBody").html('<form action=""> <input id="q1yes" type="radio" name="ans" value="yes">&nbsp;Yes<br>'+
                  '<input id="q1no" type="radio" name="ans" value="no">&nbsp;No'+
